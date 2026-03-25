16b3b6644382b9496ec542ce86f3bc2c008d2b15
arvcarl <arv.carl04@gmail.com>
2026-03-25 21:37:00 +0100

    Added calculations for multiple particles

--- scr/project.py
+++ scr/project.py
@@ -14,15 +14,30 @@ def Euler_Maruyama(x0, u, h, T, D, N):
     D  - diffusion coefficient
     N  - number of particles
     '''
-    X = x0
-    n_step = 0 # number of steps
-    while n_step*h <= T: 
-        Z = np.random.normal(0, 1, 2)
-        new_X = X[:,-1] + u*h + np.sqrt(2*D*h)*Z
-        X = np.column_stack((X, new_X))
-        n_step += 1
-    return X
+    X_N = np.zeros([2, int(T/h)+2, 0])
+    print(X_N.shape)
+    for p in range(N):
+        X = x0
+        n_step = 0 # number of steps
+        while n_step*h <= T: 
+            Z = np.random.normal(0, 1, 2)
+            new_X = X[:,-1] + u*h + np.sqrt(2*D*h)*Z
+            X = np.column_stack((X, new_X))
+            n_step += 1
+        #print(X_N.shape)
+        #X_N[p] = X
+        X_N = np.dstack((X_N, X))
+        print(X_N.shape)
+        print(X_N)
+    return X_N 
+'''
+X_N is three dimensional
 
-X = Euler_Maruyama(np.zeros([2,1]), np.array([0.0,0]), 0.1, 100, 0.2, 1)
+1: Dimensions (in this case 2)
+2: Time steps
+3: Particle number
+'''
+
+X = Euler_Maruyama(np.zeros([2,1]), np.array([0,0]), 0.1, 10, 0.1, 100)
 plt.plot(X[0,:], X[1,:])
 plt.show()
