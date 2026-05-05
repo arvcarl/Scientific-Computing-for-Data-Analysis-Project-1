847abbeaa70c137a9901d3be4a48da296d4321e7
arvcarl <arv.carl04@gmail.com>
2026-05-05 12:47:43 +0200

    Improved simulation by changing to more array based calculations. More comments and other minor changes

--- scr/project.py
+++ scr/project.py
@@ -1,11 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-
-'''from pathlib import Path
-
-this_dir = Path(__file__).resolve().parent
-sat_path = this_dir / "placeholder" '''
 
 '''
 
@@ -23,34 +17,41 @@ def Euler_Maruyama(x0, u, h, T, D, N):
     D  - diffusion coefficient
     N  - number of particles
     '''
-    X_N = np.zeros([2, int(T/h)+1, 0]) # array for position over time of every particle
-    for p in range(N):
-        X = x0
-        n_step = 0 # number of steps
-        while n_step*h < T: 
-            Z = np.random.normal(0, 1, 2)
-            new_X = X[:,-1] + u*h + np.sqrt(2*D*h)*Z # calculates position of next step
-            X = np.column_stack((X, new_X)) # appends next step to X
-            n_step += 1
-        X_N = np.dstack((X_N, X)) # appends particle X_p to array 
+
+    ''' prepares arrays and changes u and x0 to matrixes '''
+    X_N = np.zeros([2, 1, N]) # array containing the position of all particles at every time step
+    X = np.tile(x0, (1, N)) # generates array of stacked x0-vectors
+    X_N[:,0,:] = X
+    u = np.swapaxes(np.tile(u, (N, 1)), axis1=1, axis2=0) # generates array of stacked u-vectors
+    n_step = 0
+    while n_step*h < T:
+        ''' main simulation step with random numbers for diffusion in Z and new_X is the next position of all particles added to X_N '''
+        Z = np.random.normal(0, 1, (2, N)) # random number for diffusion
+        new_X = X_N[:,n_step,:] + u*h + np.sqrt(2*D*h)*Z # simulation of movement
+        new_X = np.expand_dims(new_X, 1) # adds dimension for stacking arrays
+        X_N = np.hstack((X_N, new_X)) # appends new position to total matrix
+        n_step += 1 # increments step counter
     return X_N
 '''
 X_N is a three dimensional array
 1: Spacial dimensions (in this case 2)
-2: Time steps
+2: Time step
 3: Particle number
 '''
 
 
 
 def particle_cloud(x0, u, h, plot_times, D, N):
+    ''' runs simulation and loops through plot times '''
     X = Euler_Maruyama(x0, u, h, max(plot_times), D, N) # runs simulation 
     t = (plot_times/h).astype(int) # converts time in seconds to correct step
     for p, plottime in enumerate(plot_times):
         plt.subplot(2,2,p + 1)
-        plt.scatter(X[0,t[p]],X[1,t[p]], s=5)
+        plt.scatter(X[0,t[p]],X[1,t[p]], s=2)
         plt.xlim(0,25)
         plt.ylim(-5, 5)
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
         plt.title(f'{plottime} s')
     plt.show()
 
@@ -82,7 +83,7 @@ def concentration(x0, u, h, plot_times, D, N):
         C /= N
         plt.subplot(2,2,i + 1)
         plt.title(f'{plot_times[i]} s')
-        plt.contourf(x_grid, y_grid, C, cmap='afmhot_r')
+        plt.contourf(x_grid, y_grid, C, cmap='Reds')
     plt.colorbar(ax = axes.ravel().tolist())
     plt.show()
     return x_grid, y_grid, C
@@ -99,26 +100,30 @@ def source_simulation(x0, u, h, T_sec, D, Q, plot_times):
     Q  - particles released per second
     plot_times - array of (up to) four times to show in plot
     '''
+
     t = int(T_sec/h) # total time steps
     n_particles = int(Q*h) # new particles added every time step
     X_tot = np.zeros([2, int(T_sec/h)+1, 0])
+    u = np.swapaxes(np.tile(u, (n_particles, 1)), axis1=1, axis2=0) # generates array of stacked u-vectors
     for timestep in range(t):
-        X_temp = np.zeros([2, timestep, n_particles])     # array with leading zeroes for not released particles
-        X_N = np.zeros([2, int(T_sec/h)+1 - timestep, 0]) # array for position over time of every particle
-        for p in range(n_particles):
-            X = x0
-            n_step = 0 # number of steps
-            while (n_step + timestep) *h < T_sec: # increments while elapsed time is shorter than T_sec
-                Z = np.random.normal(0, 1, 2)
-                new_X = X[:,-1] + u*h + np.sqrt(2*D*h)*Z # calculates position of next step
-                X = np.column_stack((X, new_X)) # appends next step to X
-                n_step += 1
-            X_N = np.dstack((X_N, X)) # appends particle X_p to array 
+        X_temp = np.zeros([2, timestep, n_particles]) # array with leading zeroes for not released particles
+        X_N = np.zeros([2, 1, n_particles]) # array containing the position of all particles at every time step
+        n_step = 0
+        X = x0
+        '''
+        same main simulation steps as in the first simulation, but with fewer steps for later particles and leading zeros added
+        '''
+        while (n_step + timestep) * h < T_sec:
+            Z = np.random.normal(0, 1, (2, n_particles)) # random number for diffusion
+            new_X = X_N[:,n_step,:] + u*h + np.sqrt(2*D*h)*Z # simulation of movement
+            new_X = np.expand_dims(new_X, 1) # adds dimension for stacking arrays
+            X_N = np.hstack((X_N, new_X)) # appends new position to total matrix
+            n_step += 1 # increments step counter
         X_temp = np.concatenate((X_temp, X_N), axis = 1)  # adds particles to leading zeroes
         X_tot = np.concatenate((X_tot, X_temp), axis = 2) # adds particles to total array with all particles
     X = X_tot # name change for plots
 
-    # plotting
+    '''plotting'''
     N = n_particles * t # total amount of particles
     nx, ny = 201, 101 # size of grid
     e = 0.1 # epsilon for Dirac delta approximation
@@ -136,21 +141,24 @@ def source_simulation(x0, u, h, T_sec, D, Q, plot_times):
         C /= N
         plt.subplot(2,2,i + 1)
         plt.title(f'{plot_times[i]} s')
-        plt.contourf(x_grid, y_grid, C, cmap='afmhot_r')
+        plt.contourf(x_grid, y_grid, C, cmap='Reds')
     plt.colorbar(ax = axes.ravel().tolist())
     plt.show()
     plt.subplot(2,2,1)
 
-    # same plotting as in particle_cloud
+    '''same plotting as in particle_cloud'''
     t = (plot_times/h).astype(int)
     for p, plottime in enumerate(plot_times):
         plt.subplot(2,2,p + 1)
-        plt.scatter(X[0,t[p]],X[1,t[p]], s=5)
+        plt.scatter(X[0,t[p]],X[1,t[p]], s=2)
         plt.xlim(0,25)
         plt.ylim(-5, 5)
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
         plt.title(f'{plottime} s')
     plt.show()
     return X
+
 
 
 '''Simulation parameters'''
@@ -159,8 +167,8 @@ u = np.array([0.3,0])
 h = 0.1
 T_sec = 60
 D = 0.02
-N = 100
-Q = 10
+N = 1000
+Q = 100
 plot_times = np.array([15, 30, 45, 60])
 
 '''Functions calls'''
